fa4f7ed9211f39ffe772e5733f896c9b605c5ec4
Jesse Riddle <jriddle@salesforce.com>
2021-07-01 13:15:49 -0400

    Stripped newline from password being generated. Updated output to be JSON.

--- src/password-rotation.py
+++ src/password-rotation.py
@@ -2,15 +2,21 @@
 
 import crypt
 import os
+import json
 
 # To Do: Use the xkcdpass library versus calling it from the command line.
-xkcd_command = 'xkcdpass --numwords \'6\' --case \'first\''
+xkcd_command = "xkcdpass --delimiter '-' --numwords '6' --case 'first'"
 
 stream = os.popen(xkcd_command)
 output = stream.read()
 
-new_password = output
+new_password = output.rstrip()
 new_hash = crypt.crypt(new_password, crypt.mksalt(crypt.METHOD_SHA512))
 
-print("Password: " + new_password)
-print("Hash: " + new_hash)
+secrets = {}
+secrets['password'] = new_password
+secrets['hash'] = new_hash
+
+data = json.dumps(secrets)
+
+print(data)
